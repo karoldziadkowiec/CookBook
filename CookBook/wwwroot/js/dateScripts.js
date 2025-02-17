845ac3087d982ddc28b64fc47c850b04703123a2
karoldziadkowiec <karol.dziadkowiec@gmail.com>
2025-02-17 19:30:29 +0100

    gui modified

---
+++ CookBook/wwwroot/js/dateScripts.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    // current date in navbar
+    var currentDateElement = document.getElementById("currentDate");
+    if (currentDateElement) {
+        var currentDate = new Date();
+        var options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+        currentDateElement.textContent = "Today's date: " + currentDate.toLocaleDateString(undefined, options);
+    }
+
+    // current year in footer
+    var footerYearElement = document.getElementById("currentYear");
+    if (footerYearElement) {
+        footerYearElement.textContent = new Date().getFullYear();
+    }
+});
